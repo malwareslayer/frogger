ab4e373879577630e9f0aeb8d13fc137192d7ad2
malwareslayer <dwi-mulia-mokoginta@protonmail.com>
2024-11-02 23:06:49 +0700

    update main as for standard modern tidy
    Signed-off-by: malwareslayer <dwi-mulia-mokoginta@protonmail.com>

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,14 @@
+#include "../src/interface/menu.hpp"
 #include "ncurses.h"
 
-#include "interface/menu.hpp"
-
-int main(int argc, char *argv[]) {
+auto main(int argc, char* argv[]) -> int {
     initscr();
     clear();
     noecho();
     cbreak();
     curs_set(0);
 
-    menu({
-        .status = {
-            .running = true,
-        },
-        .environment = {
-            .car = 1,
-            .log = 1
-        }
-    });
+    menu({.status = {.running = true}, .environment = {.car = 1, .log = 1}});
 
     return 0;
 }
