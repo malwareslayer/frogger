c29bc6c7764fbfe952f95565e340b64f44de2b1e
malwareslayer <dwi-mulia-mokoginta@protonmail.com>
2024-11-02 23:05:35 +0700

    update menu as for standard modern tidy
    Signed-off-by: malwareslayer <dwi-mulia-mokoginta@protonmail.com>

--- src/interface/menu.cc
+++ src/interface/menu.cc
@@ -1,26 +1,22 @@
-#include "menu.hpp"
-#include "setting.hpp"
-#include "credit.hpp"
+/*
+ * Copyright 2024 <Team 1 UPH IF 2024>
+ */
 
-#include "../property.hpp"
+#include "../../src/property.hpp"
+
+#include "../../src/interface/credit.hpp"
+#include "../../src/interface/menu.hpp"
+#include "../../src/interface/setting.hpp"
 
 const std::vector<std::string> labeling = {"Play", "Setting", "Credit", "Exit"};
 
-void menu(const CONFIGURATION &configuration) {
-    constexpr INTERFACE context = {
-        .visual = {
-            .y = 10,
-            .x = 30,
-        },
-        .interface = {
-            .choose = 1,
-        }
-    };
+void menu(const CONFIGURATION& configuration) {
+    constexpr INTERFACE context = {.visual = {.y = 10, .x = 30}, .interface = {.choose = 1}};
 
     getmaxyx(stdscr, context.visual.height, context.visual.width);
 
-    WINDOW *window = create(context.visual.y, context.visual.x,
-        (context.visual.height - context.visual.y) / 2, (context.visual.width - context.visual.x) / 2);
+    WINDOW* window = create(context.visual.y, context.visual.x, (context.visual.height - context.visual.y) / 2,
+                            (context.visual.width - context.visual.x) / 2);
 
     nodelay(window, true);
     keypad(window, true);
--- src/interface/menu.hpp
+++ src/interface/menu.hpp
@@ -1,5 +1,9 @@
+/*
+ * Copyright 2024 <Team 1 UPH IF 2024>
+ */
+
 #pragma once
 
-#include "../configuration.hpp"
+#include "../../src/configuration.hpp"
 
-void menu(const CONFIGURATION &configuration);
+void menu(const CONFIGURATION& configuration);
